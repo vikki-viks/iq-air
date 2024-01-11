b8ef909b7a09aabe5073f4dfb66cef73912c3427
vikki_viks6 <vicka.kudryavtseva@yandex.ru>
2024-01-11 16:15:09 +0100

    add docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { TelegramModule } from './telegram/telegram.module';
 import { AirModule } from './air/air.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [HttpModule, TelegramModule, AirModule],
+  imports: [HttpModule, TelegramModule, AirModule, ScheduleModule.forRoot()],
 })
 export class AppModule {}
--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -12,8 +12,6 @@ export class TelegramService {
       '6927664692:AAEvHK3pgIY0YXremYyCcPOQz-WRWQPOY64',
       { polling: true },
     );
-
-    // this.report();
     this.bot.on('message', async (msg) => {
       const text = msg.text;
       const chatId = msg.chat.id;
@@ -43,17 +41,21 @@ export class TelegramService {
 
   @Cron('1 7-23 * * *')
   private async report(lat, lon) {
-    const ownChatId = -1002072527250;
-    const result = await this.airService.findCountry(lat, lon);
-    const index = (result as any).data.current.pollution.aqius;
-    console.log(result);
-    if (index < 100) {
-      this.bot.sendMessage(
-        ownChatId,
-        `Воздух норм, проветри\nИндекс: ${index}`,
-      );
-    } else {
-      this.bot.sendMessage(ownChatId, `Закрывай окна\nИндекс: ${index}`);
+    try {
+      const ownChatId = -1002072527250;
+      const result = await this.airService.findCountry(lat, lon);
+      const index = (result as any).data.current.pollution.aqius;
+      console.log(result);
+      if (index < 100) {
+        this.bot.sendMessage(
+          ownChatId,
+          `Воздух норм, проветри\nИндекс: ${index}`,
+        );
+      } else {
+        this.bot.sendMessage(ownChatId, `Закрывай окна\nИндекс: ${index}`);
+      }
+    } catch (error) {
+      return error;
     }
   }
 }
